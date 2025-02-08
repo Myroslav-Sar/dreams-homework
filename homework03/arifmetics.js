660275bc248146986989a8bcba58aa185de43bb1
Myroslav Saratovkin <saratovkin.myroslav@gmail.com>
2025-02-08 17:06:55 +0200

    arifmetics, decision-tree, logical, switch, and package.json are added

---
+++ homework03/arifmetics.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 5;
+let str = "20";
+let bool = true;
+ 
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("String and number addition:", str + num1);
+console.log("Boolean and number addition:", bool + num1);
